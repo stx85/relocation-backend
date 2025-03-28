d1998a652e05a89d679d2c60896cd24369b97559
Stefan Hahnl <hahnl.stefan@gmail.com>
2025-03-28 11:27:43 +0100

    chor: initialize project

---
+++ src/main/java/fh/bswe/relocationbackend/RelocationBackendApplication.java
@@ -0,0 +1,13 @@
+package fh.bswe.relocationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelocationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelocationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fh/bswe/relocationbackend/RelocationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.bswe.relocationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelocationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
